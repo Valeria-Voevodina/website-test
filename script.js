08b85612802c8a81ff5517e33615ff2e98aecb93
valeria_voevodina_01 <167454160+Valeria-Voevodina@users.noreply.github.com>
2025-10-18 01:19:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -25,11 +25,3 @@ window.addEventListener('mouseup', () => {
   isResizing = false;
   document.body.style.cursor = 'default';
 });
-
-const frame1 = document.querySelector('#col1 iframe');
-const frame2 = document.querySelector('#col2 iframe');
-
-frame1.addEventListener('scroll', () => {
-  frame2.scrollTop = frame1.scrollTop;
-});
-
